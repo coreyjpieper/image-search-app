764d852df825d6693a0da705610523648caa47f0
Corey <coreyjpieper@gmail.com>
2019-05-29 18:51:30 -0500

    Asynchronously show number of images returned by search

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,16 @@ import SearchBar from './SearchBar';
 const ACCESS_KEY = "2b41fc64e677aec6b6c06f35372134d2421ed97751c525760e7a6a6e35ec0ce0";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = term => {
+    axios.get('/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID ' + ACCESS_KEY
       }
+    }).then((response) => {
+      this.setState({ images: response.data.results });
     });
   }
 
@@ -18,6 +22,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '20px'}}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>);
   }
 }

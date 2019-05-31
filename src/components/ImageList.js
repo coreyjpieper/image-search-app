d3c39056327dc1934215ec1a63d24013649dc2c1
Corey <coreyjpieper@gmail.com>
2019-05-30 19:20:53 -0500

    Added alt and key property to image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const ImageList = props => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />
+  const images = props.images.map(( { alt_description, id, urls }) => {
+    return <img alt={alt_description} key={id} src={urls.regular} />
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
